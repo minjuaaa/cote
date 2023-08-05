23d96342162fd3283b1389d5b693c808e8e17641
minjuaaa <51702223+minjuaaa@users.noreply.github.com>
2023-08-05 23:21:46 +0900

    Add files via upload

---
+++ 10799.py
@@ -0,0 +1,22 @@
+s = input()
+stack=[]
+now = 0
+count = 0
+leng = len(s)
+
+for i in range(leng):
+    if i < leng-1:
+        if s[i] == '(' and s[i+1] == ')':
+            count += now
+            now += 1
+
+            continue
+
+    if s[i] == ')':
+        now -= 1
+
+    elif s[i] == '(':
+        now += 1
+        count += 1
+
+print(count)
